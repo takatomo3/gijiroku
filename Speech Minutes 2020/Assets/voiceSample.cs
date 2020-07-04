166d45697a225efc0128f7eead50ddb2d8677532
mlab2020agile <mlab2020agile@gmail.com>
2020-07-04 16:12:54 +0900

    栗間・小林 音声認識を実装しました 音声認識した言葉を、テキストファイルに書き込むようにしました

--- Speech Minutes 2020/Assets/voiceSample.cs
+++ Speech Minutes 2020/Assets/voiceSample.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class voiceSample : MonoBehaviour
 {
@@ -8,32 +9,79 @@ public class voiceSample : MonoBehaviour
     private PXCMAudioSource source;
     private PXCMSpeechRecognition sr;
     private PXCMSpeechRecognition.Handler handler;
+    bool Record = true;
+    public string outputtext;
+    string filePath;
+
 
     public void Start()
     {
-        session = PXCMSession.CreateInstance();
-        source = session.CreateAudioSource();
+        filePath = Application.dataPath + @"\LogData.txt";
+        File.CreateText(filePath);
 
-        PXCMAudioSource.DeviceInfo dinfo = null;
+        //ブラックボックス
+            session = PXCMSession.CreateInstance();
+            source = session.CreateAudioSource();
 
-        //デバイスを検出して出力
-        source.QueryDeviceInfo(0, out dinfo);
-        source.SetDevice(dinfo);
-        Debug.Log(dinfo.name);
+            PXCMAudioSource.DeviceInfo dinfo = null;
 
-        session.CreateImpl<PXCMSpeechRecognition>(out sr);
+            //デバイスを検出して出力
+            source.QueryDeviceInfo(0, out dinfo);
+            source.SetDevice(dinfo);
+            Debug.Log(dinfo.name);
 
-        PXCMSpeechRecognition.ProfileInfo pinfo;
-        sr.QueryProfile(out pinfo);
-        pinfo.language = PXCMSpeechRecognition.LanguageType.LANGUAGE_JP_JAPANESE;
-        sr.SetProfile(pinfo);
+            session.CreateImpl<PXCMSpeechRecognition>(out sr);
 
-        handler = new PXCMSpeechRecognition.Handler();
-        handler.onRecognition = (x) => Debug.Log(x.scores[0].sentence);
-        sr.SetDictation();
-        sr.StartRec(source, handler);
+            PXCMSpeechRecognition.ProfileInfo pinfo;
+            sr.QueryProfile(out pinfo);
+            pinfo.language = PXCMSpeechRecognition.LanguageType.LANGUAGE_JP_JAPANESE;
+            sr.SetProfile(pinfo);
+
+            handler = new PXCMSpeechRecognition.Handler();
+            //handler.onRecognition = (x) => Debug.Log(x.scores[0].sentence);
+            sr.SetDictation();
+            //sr.StartRec(source, handler);
+        //ブラックボックスここまで
+
     }
 
+    //データの出力
+    void Dataoutput(string text)
+    {
+        //StreamWriter型のswを宣言
+        StreamWriter sw = new StreamWriter(filePath,true);
+        //改行
+        sw.Write(text+"\n");
+        sw.Close();
+        //ログの書き出し
+        Debug.Log(text);
+        outputtext = text;
+        
+
+    }
+
+    //Recordボタンを押すと呼び出されるメソッド
+    public void SetRecord()
+    {
+        //Recordがtrueなら(最初に押されたら)
+        if (Record)
+        {
+
+            handler.onRecognition = (x) => Dataoutput(x.scores[0].sentence);    //わかりません
+            sr.StartRec(source, handler);                                       //音声認識の開始
+            Record = false;                                                     //Recordをfalseにする
+            Debug.Log("Record True");                                           //デバッグログ
+        }
+        //Recordがfalseなら(音声認識中に押されたら)
+        else
+        {
+            sr.StopRec();               //音声認識の終了
+            Record = true;              //Recordをtrueにする
+            Debug.Log("Record False");  //デバッグログ
+        }
+    }
+
+    //ブラックボックス
     void OnDisable()
     {
         if (sr != null)
