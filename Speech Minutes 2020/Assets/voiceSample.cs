57bdd94de7def8fe1bdf93ff38810329c75ac4cb
takatomo3 <csta17040@g.nihon-u.ac.jp>
2020-07-04 14:08:55 +0900

    音声認識サンプル導入

---
+++ Speech Minutes 2020/Assets/voiceSample.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class voiceSample : MonoBehaviour
+{
+    private PXCMSession session;
+    private PXCMAudioSource source;
+    private PXCMSpeechRecognition sr;
+    private PXCMSpeechRecognition.Handler handler;
+
+    public void Start()
+    {
+        session = PXCMSession.CreateInstance();
+        source = session.CreateAudioSource();
+
+        PXCMAudioSource.DeviceInfo dinfo = null;
+
+        //デバイスを検出して出力
+        source.QueryDeviceInfo(0, out dinfo);
+        source.SetDevice(dinfo);
+        Debug.Log(dinfo.name);
+
+        session.CreateImpl<PXCMSpeechRecognition>(out sr);
+
+        PXCMSpeechRecognition.ProfileInfo pinfo;
+        sr.QueryProfile(out pinfo);
+        pinfo.language = PXCMSpeechRecognition.LanguageType.LANGUAGE_JP_JAPANESE;
+        sr.SetProfile(pinfo);
+
+        handler = new PXCMSpeechRecognition.Handler();
+        handler.onRecognition = (x) => Debug.Log(x.scores[0].sentence);
+        sr.SetDictation();
+        sr.StartRec(source, handler);
+    }
+
+    void OnDisable()
+    {
+        if (sr != null)
+        {
+            sr.StopRec();
+            sr.Dispose();
+        }
+
+        if (session != null)
+            session.Dispose();
+    }
+}
