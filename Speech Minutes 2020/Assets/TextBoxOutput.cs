cc43bca3039e0836e404dbc01f260fde03927c84
Takatomo <csta17040@g.nihon-u.ac.jp>
2020-09-17 15:39:59 +0900

    テキストの入力、表示を可能にした。 次回はテキストボックスの移動と削除をやります。

---
+++ Speech Minutes 2020/Assets/TextBoxOutput.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextBoxOutput : MonoBehaviour
+{
+    //オブジェクトと結びつける
+    public InputField inputField;
+    public Text text;
+
+    public GameObject canvas;//キャンバス
+    public GameObject gametext;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Componentを扱えるようにする
+        inputField = inputField.GetComponent<InputField>();
+        text = text.GetComponent<Text>();
+    }
+
+    public void InputText()
+    {
+        //テキストにinputFieldの内容を反映
+        text.text = inputField.text;
+        gametext.gameObject.SetActive(true);
+    }
+
+    public void EndEdit()
+    {
+        GameObject prefab = (GameObject)Instantiate(gametext);
+        prefab.transform.SetParent(canvas.transform, false);
+    }
+}
