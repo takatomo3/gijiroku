9f6d8144c4a9a888e7ba4317a8024672ae9f2f09
Takatomo <csta17040@g.nihon-u.ac.jp>
2020-09-18 17:40:00 +0900

    テキストボックスをマウスで移動できるようにしました。

--- Speech Minutes 2020/Assets/TextControl.cs
+++ Speech Minutes 2020/Assets/TextControl.cs
@@ -1,14 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.EventSystems;
 using UnityEngine;
 
-public class TextControl : MonoBehaviour
+public class TextControl : MonoBehaviour, IDragHandler
 {
     public void Update()
     {
         
     }
 
+    public RectTransform m_rectTransform = null;
+
+    private void Reset()
+    {
+        m_rectTransform = GetComponent<RectTransform>();
+    }
+
+    public void OnDrag(PointerEventData e)
+    {
+        m_rectTransform.position += new Vector3(e.delta.x, e.delta.y, 0f);
+    }
+
+    //テキストボックスの削除(バックスペースで削除)
     public void Destroy()
     {
         if (Input.GetKey(KeyCode.Backspace)) {
--- Speech Minutes 2020/Assets/TextManager.cs
+++ Speech Minutes 2020/Assets/TextManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class TextBoxOutput : MonoBehaviour
+public class TextManager : MonoBehaviour
 {
     //オブジェクトと結びつける
     public InputField inputField;
@@ -24,19 +24,24 @@ public class TextBoxOutput : MonoBehaviour
     {
         //テキストにinputFieldの内容を反映
         text.text = inputField.text;
+        //オブジェクトを表示する
         gametext.gameObject.SetActive(true);
     }
 
+    //テキストボックスへの入力が終わった時に呼び出す
     public void EndEdit()
     {
         if (GameObject.Find("TextBox").GetComponent<InputField>().text != "") {
+            //テキストがあればプレハブからオブジェクト生成
             GameObject prefab = (GameObject)Instantiate(gametext);
             prefab.transform.SetParent(canvas.transform, false);
         }
     }
 
+    //送信ボタンを押した時
     public void Send()
     {
+        //インプットフィールドの中身を消す
         GameObject.Find("TextBox").GetComponent<InputField>().text = "";
     }
 }
