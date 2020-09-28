ceee5b270433a2141a0b95c7c905bb83317204c4
mlab2020agile <mlab2020agile@gmail.com>
2020-09-28 11:33:12 +0900

    安藤 栗間 岩鶴　UIを実装と、機能を実装しました　

--- Speech Minutes 2020/Assets/UIManager.cs
+++ Speech Minutes 2020/Assets/UIManager.cs
@@ -6,28 +6,51 @@ public class UIManager : MonoBehaviour
 {
     [SerializeField] GameObject WadaiPanel;
     [SerializeField] GameObject WhiteBoardPanel;
+    [SerializeField] GameObject SendTextPanel;
     // Start is called before the first frame update
 
     bool EnableWadaiPanel = true;
     bool EnableWhiteBoardPanel = true;
+    bool EnableSendTextPanel = true;
 
     void Start()
     {
         EnableWadaiPanel = true;
         EnableWhiteBoardPanel = true;
+        EnableSendTextPanel = true;
     }
 
     public void OnclickWadaiButton()
     {
+        WhiteBoardPanel.SetActive(false);
+        SendTextPanel.SetActive(false);
+        EnableWhiteBoardPanel = true;
+        EnableSendTextPanel = true;
+
         WadaiPanel.SetActive(EnableWadaiPanel);
         EnableWadaiPanel = !EnableWadaiPanel;
     }
 
     public void OnclickWhiteBoardButton()
     {
+        WadaiPanel.SetActive(false);
+        SendTextPanel.SetActive(false);
+        EnableWadaiPanel = true;
+        EnableSendTextPanel = true;
+
         WhiteBoardPanel.SetActive(EnableWhiteBoardPanel);
         EnableWhiteBoardPanel = !EnableWhiteBoardPanel;
+    }
 
+    public void OnclickSendTextButton()
+    {
+        WadaiPanel.SetActive(false);
+        WhiteBoardPanel.SetActive(false);
+        EnableWadaiPanel = true;
+        EnableWhiteBoardPanel = true;
+
+        SendTextPanel.SetActive(EnableSendTextPanel);
+        EnableSendTextPanel = !EnableSendTextPanel;
     }
 
 
